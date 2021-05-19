148f5fba3327a65215791784858b69434f61051e
Michelle Phan <mphan2868@hotmail.com>
2021-05-19 15:02:58 +1000

    Setup stack navigator for IndexScreen

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native'
+
+const IndexScreen = () => {
+
+    return(
+        <Text>IndexScreen</Text>
+    )
+    
+}
+
+const styles= StyleSheet.create ({})
+
+export default IndexScreen
